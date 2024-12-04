9b9b617d8a5a09c169c50e4944422abe56167b03
M1khail <128730637+M1hasMAD@users.noreply.github.com>
2024-12-04 20:17:53 +0100

    commit

--- Github/Main.cs
+++ Github/Main.cs
@@ -4,6 +4,11 @@ public class Main
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!"); // Print "Hello World!"
+    } // Main
+    
+    public static void Main2(string[] args)
+    {
+        Console.WriteLine("Hello World!"); // Print "Hello World!"
     }
 }
