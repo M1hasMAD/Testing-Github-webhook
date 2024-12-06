5c14aa96b6bc6d7def2067198951690229aa8d2a
M1khail <128730637+M1hasMAD@users.noreply.github.com>
2024-12-06 14:38:59 +0100

    bruh

--- Github/Main.cs
+++ Github/Main.cs
@@ -10,5 +10,9 @@ public class Main
     public static void Main2(string[] args)
     {
         Console.WriteLine("Hello World!"); // Print "Hello World!"
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
     }
 }
